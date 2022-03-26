5d55e9c260d79e5aac806d1626511a07a3d9120e
artem_savchenko <savcenkoartem334@gmail.com>
2022-03-26 17:17:40 +0300

    lesson7 home task

--- lesson7.py
+++ lesson7.py
@@ -15,16 +15,20 @@ class Matrix:
 
     def __add__(self, other):
         try:
+            x = []
             for i in range(len(self.matrix)):
                 if len(self.matrix) != len(other.matrix):
                     raise MyError('Матрицы различаются по числу строк!')
+                y = []
                 for j in range(len(self.matrix[i])):
                     if len(self.matrix[i]) != len(other.matrix[i]):
                         raise MyError('Матрицы различаются по числу столбцов!')
-                    self.matrix[i][j] += other.matrix[i][j]
-            return Matrix(self.matrix)
+                    y.append(self.matrix[i][j] + other.matrix[i][j])
+                x.append(y)
+            return Matrix(x)
         except MyError as mr:
             print(mr)
+
 
 obj = Matrix([[1,2,3,4],[1,2,3,2],[2,3,2,3]])
 obj2 = Matrix([[1,2,3,4],[1,2,3,2],[2,3,2,3]])
